af345df9100571d91cae82e1e5405ecd3aca609f
Viktorija Paukova <vicpa34@yahoo.com>
2024-01-11 23:04:50 +0200

    Second module

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const router = require('./src/router.js');
 
 const app = express();
 
 const pathToIndex = path.resolve(_dirname, '../client/index.html');
 
-app.use();
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 module.exports = app;
