70a74b8c353b53545d2ec1b730df5cdf91bdb4f8
Viktorija Paukova <vicpa34@yahoo.com>
2024-01-11 22:14:52 +0200

    module one

---
+++ api/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const pathToIndex = path.resolve(_dirname, '../client/index.html');
+
+app.use();
+
+module.exports = app;
